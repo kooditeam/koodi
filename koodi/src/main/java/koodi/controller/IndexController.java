3a1d5462209691cef9859f6ea2a64e857b474696
Kangas Juha P <jpkangas@dhp-82.cs.helsinki.fi>
2015-10-28 12:26:36 +0200

    Initial

---
+++ koodi/src/main/java/koodi/controller/IndexController.java
@@ -0,0 +1,15 @@
+package koodi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("*")
+    public String hello(Model model) {
+        model.addAttribute("hello", "world");
+        return "index";
+    }
+}
