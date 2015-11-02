19ee414198eb7a20f95c1a61f7cbbe424d53704a
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-11-02 23:57:48 +0200

    added interface for dealing with the database for Answer entity

---
+++ koodi/src/main/java/koodi/repository/AnswerRepository.java
@@ -0,0 +1,9 @@
+
+package koodi.repository;
+
+import koodi.domain.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    
+}
