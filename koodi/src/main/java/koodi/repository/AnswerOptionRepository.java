395ad9d360d9a6455483b1aa4e139362f1607c60
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-11-11 22:20:40 +0200

    created repositories for AnswerOption, Question and QuestionSeries

---
+++ koodi/src/main/java/koodi/repository/AnswerOptionRepository.java
@@ -0,0 +1,8 @@
+
+package koodi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerOptionRepository extends JpaRepository<AnswerOption, Long> {
+    
+}
