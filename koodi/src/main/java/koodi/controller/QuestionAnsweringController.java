9f6a2fe30a9a905ab1d5334eb2c8262eddfe8ed5
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2015-11-16 15:33:08 +0200

    question post works in a basic level

--- koodi/src/main/java/koodi/controller/QuestionAnsweringController.java
+++ koodi/src/main/java/koodi/controller/QuestionAnsweringController.java
@@ -1,12 +1,15 @@
 
 package koodi.controller;
 
+import koodi.domain.AnswerOption;
 import koodi.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("tehtavat")
@@ -19,5 +22,12 @@ public class QuestionAnsweringController {
     public String list(Model model) {
         model.addAttribute("questions", questionService.findAll());
         return "answer_questions";
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public String postAnswer(@ModelAttribute AnswerOption answerOption) {
+        System.out.println("The answer was: " + answerOption.getAnswerText());
+        
+        return "redirect:/tehtavat";
     }
 }
