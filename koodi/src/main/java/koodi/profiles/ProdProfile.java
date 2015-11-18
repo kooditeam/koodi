55d64872d96eb23cb650048d37984c2fe3c3782a
pekoe09 <juha.kangas@welho.com>
2015-11-18 19:50:07 +0200

    Added second TemplateResolver to sort out heroku/thymeleaf template visibility

--- koodi/src/main/java/koodi/profiles/ProdProfile.java
+++ koodi/src/main/java/koodi/profiles/ProdProfile.java
@@ -1,11 +1,28 @@
 
 package koodi.profiles;
 
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @Profile("prod")
 public class ProdProfile {
     
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+    
+    @PostConstruct
+    public void extendTemplateResolvers(){
+        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();        
+        resolver.setPrefix("/koodi/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(templateEngine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        templateEngine.addTemplateResolver(resolver);
+    }
 }
