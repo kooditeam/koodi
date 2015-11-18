27cbea16f8b73db2d76eda8ff2ee15ec00da12fe
pekoe09 <juha.kangas@welho.com>
2015-11-18 20:54:41 +0200

    Trying out TemplateResolver

---
+++ koodi/src/main/java/koodi/TemplateResolverExtender.java
@@ -0,0 +1,25 @@
+package koodi;
+
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+public class TemplateResolverExtender {
+    
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+    
+    @PostConstruct
+    public void extendTemplateResolvers(){
+        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();        
+        resolver.setPrefix("/koodi/src/main/webapp/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(templateEngine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        templateEngine.addTemplateResolver(resolver);
+    }
+}
