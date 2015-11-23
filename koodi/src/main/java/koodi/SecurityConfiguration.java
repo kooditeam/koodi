9d501ed3e03516d33a929f0c0a3b7721fe7ee503
pekoe09 <juha.kangas@welho.com>
2015-11-23 16:17:02 +0200

    Fixed deprecated EnableWebMvcSecurity and temp-fixed ajax-csrf-issue

--- koodi/src/main/java/koodi/SecurityConfiguration.java
+++ koodi/src/main/java/koodi/SecurityConfiguration.java
@@ -5,16 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http.csrf().disable();
         http.authorizeRequests()
                 .anyRequest().permitAll();
 //                .antMatchers("/login", "/rekisteroityminen", "index").permitAll()
