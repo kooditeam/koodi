b244f2890b044aba895cc0eaf354ec02e7885208
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2015-11-28 13:23:23 +0200

    minor refactoring (after debugging

--- koodi/src/main/java/koodi/service/QuestionService.java
+++ koodi/src/main/java/koodi/service/QuestionService.java
@@ -14,26 +14,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class QuestionService extends BaseService {
-    
+
     @Autowired
     private QuestionRepository questionRepository;
-    
+
     @Autowired
     private AnswerOptionRepository answerOptionRepository;
-    
+
     public List<Question> findAll() {
         return questionRepository.findAll();
     }
-    
+
     public List<Question> findByQuestionSeries(QuestionSeries questionSeries) {
         return questionRepository.findByQuestionSeries(questionSeries);
     }
-    
-    public Question findById(Long id){
+
+    public Question findById(Long id) {
         return questionRepository.findOne(id);
     }
-    
-    public void delete(Long id){
+
+    public void delete(Long id) {
         questionRepository.delete(id);
     }
 
@@ -41,24 +41,23 @@ public class QuestionService extends BaseService {
         super.save(question, null);
         questionRepository.save(question);
     }
-    
+
     public void postNewExercise(Question question, String rightAnswer,
             String falseAnswers) {
-             
+
         String[] falseOptionStrings = falseAnswers.split(";");
-        
+
         List<AnswerOption> allOptions = parseFalseOptionStrings(falseOptionStrings);
         allOptions.add(parseCorrectAnswer(rightAnswer));
-        
+
         saveOptionsInRandomOrder(question, allOptions);
-        
+
         save(question);
-        
     }
-    
+
     private void saveOptionsInRandomOrder(Question question,
             List<AnswerOption> allOptions) {
-        
+
         long seed = System.nanoTime();
         Collections.shuffle(allOptions, new Random(seed));
         for (AnswerOption option : allOptions) {
@@ -66,22 +65,21 @@ public class QuestionService extends BaseService {
         }
         question.setAnswerOptions(allOptions);
     }
-    
+
     private List<AnswerOption> parseFalseOptionStrings(String[] falseOptionStrings) {
-        
+
         List<AnswerOption> allFalseOptions = new ArrayList<>();
-        
+
         for (String falseOptionString : falseOptionStrings) {
             AnswerOption falseOption = new AnswerOption();
             falseOption.setAnswerText(falseOptionString.trim());
-            
+
             allFalseOptions.add(falseOption);
         }
-        
         return allFalseOptions;
-        
+
     }
-    
+
     private AnswerOption parseCorrectAnswer(String correctAnswer) {
         AnswerOption correct = new AnswerOption();
         correct.setAnswerText(correctAnswer.trim());
