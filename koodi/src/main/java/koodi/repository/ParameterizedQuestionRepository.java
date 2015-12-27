5aa78c100eb60f8433e1a73fbcd55eacd42ecba0
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2015-12-27 13:08:24 +0200

    added repository for parameterized questions

---
+++ koodi/src/main/java/koodi/repository/ParameterizedQuestionRepository.java
@@ -0,0 +1,8 @@
+
+package koodi.repository;
+
+import koodi.domain.ParameterizedQuestion;
+
+public interface ParameterizedQuestionRepository extends BaseRepository<ParameterizedQuestion> {
+    
+}
