ba58f55285a8d71eee2cdbd73b8cff44b8514251
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-06-02 21:14:49 +0300

    fix for the app to connect to PostgreSQL in Heroku

--- src/main/java/koodi/profiles/ProdProfile.java
+++ src/main/java/koodi/profiles/ProdProfile.java
@@ -1,44 +1,31 @@
 
 package koodi.profiles;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
 import javax.annotation.PostConstruct;
-import koodi.domain.AnswerOption;
-import koodi.domain.Question;
-import koodi.domain.QuestionSeries;
+import javax.sql.DataSource;
 import koodi.domain.User;
 import koodi.repository.AnswerOptionRepository;
 import koodi.repository.QuestionRepository;
 import koodi.repository.QuestionSeriesRepository;
 import koodi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @Profile("prod")
 public class ProdProfile {
-//    
-//        @Bean
-//    public BasicDataSource dataSource() throws URISyntaxException {
-//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//
-//        String username = dbUri.getUserInfo().split(":")[0];
-//        String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-//
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//
-//        return basicDataSource;
-//    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
     
     @Autowired
     private UserRepository userRepository;
